d7bc61f808ef9eea2eeedb5b2f561ee836e6ac23
krauserhuang <crescentdence@gmail.com>
2020-08-24 15:24:19 +0800

    2020/08/24 15:23 update mask image

--- ColorChangePractice/ViewController.swift
+++ ColorChangePractice/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //image有3個，作為背景的pattern/作為漸層的view/最上層的image
+    //backGroundView作為alpha歸零顯現
     @IBOutlet weak var backGroundView: UIImageView!
-    @IBOutlet weak var gradientView: UIImageView!
-    @IBOutlet weak var eyesView: UIImageView!
-    @IBOutlet weak var hairView: UIImageView!
+    //漸層背景
+    @IBOutlet weak var gradientView: UIView!
+    //拿來去背做遮罩的hair/eyes
+    @IBOutlet weak var hairView: UIView!
+    @IBOutlet weak var eyesView: UIView!
     //red/green/blue/alpha/gradient/corner radius的所有slider
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var redSliderValue: UILabel!
@@ -28,60 +30,89 @@ class ViewController: UIViewController {
     //切換頭髮變色或者眼睛變色
     @IBOutlet weak var segmentControl: UISegmentedControl!
     
-    let gradientLayer = CAGradientLayer()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //一開始先讓主層以外的背景透明化
-
+        let hairImageView = UIImageView(image: UIImage(named: "Amiya hair mask"))
+        hairImageView.frame = hairView.bounds
+        hairImageView.contentMode = .scaleAspectFit
+        hairView.mask = hairImageView
+        let eyesImageView = UIImageView(image: UIImage(named: "Amiya eyes mask"))
+        eyesImageView.frame = eyesView.bounds
+        eyesImageView.contentMode = .scaleAspectFit
+        eyesView.mask = eyesImageView
     }
+    
     @IBAction func sliderChange(_ sender: UISlider) {
         //讓red~alpha slider旁的數值可以跟著slider走，%.2f為顯示到小數點第二位
         redSliderValue.text = String(format: "%.2f", redSlider.value)
         greenSliderValue.text = String(format: "%.2f", greenSlider.value)
         blueSliderValue.text = String(format: "%.2f", blueSlider.value)
         alphaSliderValue.text = String(format: "%.1f", alphaSlider.value)
-        
+        //切換segment來控制更動眼睛或頭髮的顏色
         let segmentSelect = segmentControl.selectedSegmentIndex
         switch segmentSelect {
         case 0:
             //hairView的背景色跟著slider走
             hairView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
         case 1:
-            hairView.alpha = 0
+//            hairView.alpha = 0
+            //eyesView的背景色跟著slider走
             eyesView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
         default:
             return
         }
-        
-        
-
-        
-
-    }
-    @IBAction func gradientSliderChange(_ sender: UISlider) {
-        //設定漸層
-//        let gradientLayer = CAGradientLayer()
-        let gradientLocation = round(15 * gradientSlider.value) / 15
-        gradientLayer.frame = gradientView.bounds
-//        gradientLayer.colors = [CGColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))]
-        gradientLayer.colors = [UIColor.cyan.cgColor,UIColor.magenta.cgColor]
-//        gradientLayer.colors = [UIColor.red.cgColor, UIColor.orange.cgColor, UIColor.yellow.cgColor, UIColor.green.cgColor, UIColor.blue.cgColor, UIColor.purple.cgColor]
-        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
-        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
-        gradientLayer.locations = [0.0, NSNumber(value: gradientLocation)]
-        gradientView.layer.addSublayer(gradientLayer)
-        hairView.addSubview(gradientView)
     }
     
-    @IBAction func randomButton(_ sender: Any) {
+    @IBAction func gradientSliderChange(_ sender: UISlider) {
+        //漸層前需設定屬於漸層的CAGradientLayer()
+        let gradientLayer = CAGradientLayer()
+//        let gradientLocation = round(15 * gradientSlider.value) / 15
+        gradientLayer.frame = gradientView.bounds
+        gradientLayer.frame = hairView.bounds
+        gradientLayer.frame = eyesView.bounds
+////        gradientLayer.colors = [CGColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))]
+//        gradientLayer.colors = [UIColor.cyan.cgColor,UIColor.magenta.cgColor]
+////        gradientLayer.colors = [UIColor.red.cgColor, UIColor.orange.cgColor, UIColor.yellow.cgColor, UIColor.green.cgColor, UIColor.blue.cgColor, UIColor.purple.cgColor]
+//        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+//        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+//        gradientLayer.locations = [0.0, NSNumber(value: gradientSlider.value)]
+//        gradientView.layer.addSublayer(gradientLayer)
+//        hairView.addSubview(gradientView)
+//        eyesView.addSubview(gradientView)
+        let segmentSelect = segmentControl.selectedSegmentIndex
+        switch segmentSelect {
+        case 0:
+//        gradientLayer.colors = [CGColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))]
+            gradientLayer.colors = [UIColor.cyan.cgColor,UIColor.magenta.cgColor]
+//        gradientLayer.colors = [UIColor.red.cgColor, UIColor.orange.cgColor, UIColor.yellow.cgColor, UIColor.green.cgColor, UIColor.blue.cgColor, UIColor.purple.cgColor]
+            gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+            gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+            gradientLayer.locations = [0.0, NSNumber(value: gradientSlider.value)]
+            gradientView.layer.addSublayer(gradientLayer)
+            hairView.addSubview(gradientView)
+        case 1:
+//        gradientLayer.colors = [CGColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))]
+            gradientLayer.colors = [UIColor.cyan.cgColor,UIColor.magenta.cgColor]
+//        gradientLayer.colors = [UIColor.red.cgColor, UIColor.orange.cgColor, UIColor.yellow.cgColor, UIColor.green.cgColor, UIColor.blue.cgColor, UIColor.purple.cgColor]
+            gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+            gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+            gradientLayer.locations = [0.0, NSNumber(value: gradientSlider.value)]
+            gradientView.layer.addSublayer(gradientLayer)
+            eyesView.addSubview(gradientView)
+        default:
+            return
+        }
+    }
+    
+    @IBAction func randomButton(_ sender: UIButton) {
         //讓red~alpha slider旁的數值可以跟著slider走，%.2f為顯示到小數點第二位
         redSliderValue.text = String(format: "%.2f", redSlider.value)
         greenSliderValue.text = String(format: "%.2f", greenSlider.value)
         blueSliderValue.text = String(format: "%.2f", blueSlider.value)
         alphaSliderValue.text = String(format: "%.1f", alphaSlider.value)
-        
+        //random鈕在需要切換hair/eyes都要能作動
         let segmentSelect = segmentControl.selectedSegmentIndex
         switch segmentSelect {
         case 0:
@@ -94,7 +125,6 @@ class ViewController: UIViewController {
             hairView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
 //            eyesView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 0)
         case 1:
-            hairView.alpha = 0
             //用.random來設定各個slider的變數
             redSlider.value = Float.random(in: 0...1)
             greenSlider.value = Float.random(in: 0...1)
@@ -107,7 +137,4 @@ class ViewController: UIViewController {
             return
         }
     }
-    
-
 }
-
